401a4e8d1e09f19a1bc724cc8111cd94058c1a68
kevin kim <sycodes95@gmail.com>
2024-02-17 13:32:48 -0800

    methods done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,36 @@
-﻿namespace Giraffe1 {
+﻿namespace Calculator {
   class Program {
     static void Main(string[] args) {
-      "testing changes"
+      
+    }
+    private static int Total = 0;
+
+    public static int Add (int num)
+    {
+      return Total += num;
+    }
+
+    public static int Subtract(int num)
+    {
+      return Total -= num;
+
+    }
+
+    public static int Multiply(int num)
+    {
+      return Total *= num;
+    }
+
+    public static int Divide(int num)
+      
+    {
+      if(num == 0) return 0;  
+      return Total /= num; 
+    }
+
+    public static void ResetTotal () 
+    {
+      Total = 0;  
     }
   }
 }
